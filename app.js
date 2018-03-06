18b20527527ff9cd7aa5b014202c8d5b0020508d
Radhika Mattoo <rm3485@nyu.edu>
2018-03-05 20:40:52 -0500

    setup handler

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('trust proxy', true);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,9 +24,26 @@ app.get('/', function(req,res){
   res.send("Home!");
 });
 
-
 app.get('/data', function(req,res){
-  res.send("Data!");
+  res.send('Data!');
+});
+
+app.post('/data', function(req,res){
+  const ret = {};
+  const username = req.body.username;
+  const password = req.body.password;
+  const ip = req.headers["x-forwarded-for"];
+  if (ip){
+    const list = ip.split(",");
+    ip = list[list.length-1];
+  } else {
+    ip = req.connection.remoteAddress;
+  }
+  ret.username = username;
+  ret.password = password;
+  ret.ip = ip;
+  console.log("return object:", ret);
+  res.send(ret);
 });
 
 // catch 404 and forward to error handler
