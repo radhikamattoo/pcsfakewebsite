132fe4b4d489e9787250e139956487248d5b45cb
Radhika Mattoo <rm3485@nyu.edu>
2018-03-05 20:45:17 -0500

    more verbose error message

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.send('error');
+    res.send(err);
   });
 }
 
@@ -68,7 +68,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send('error');
+  res.send(err);
 });
 
 
